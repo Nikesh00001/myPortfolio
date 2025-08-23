9a4666709d07e822bbb63648dba69c900da5c107
nikesh gautam <nikeshgautam2059@gmail.com>
2025-08-24 01:52:02 +0545

    fixed layout importing case

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Children } from "react";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import Home from "../pages/Home";
 import Footer from "../components/Footer";
 import Message from "../components/message";
